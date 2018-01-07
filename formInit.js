a2279195ec9c63ad3319ba08c4eafd113c580c60
Gary Chi <gc1753@nyu.edu>
2018-01-07 14:51:15 -0500

    reorganized

---
+++ formInit.js
@@ -0,0 +1,38 @@
+function getFormData() {
+    var elements = document.getElementById("cForm").elements; // all form elements
+    var fields = Object.keys(elements).map(function (k) {
+        if (elements[k].name !== undefined) {
+            return elements[k].name;
+        }
+    }).filter(function (item, pos, self) {
+        return self.indexOf(item) == pos && item;
+    });
+    var data = {};
+    fields.forEach(function (k) {
+        data[k] = elements[k].value;
+    });
+    return data;
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    var data = getFormData(); // get the values submitted in the form
+    var url = event.target.action; //
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function () {
+        // add function to display successful
+        return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function (k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    xhr.send(encoded);
+}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    var form = document.getElementById('cForm');
+    form.addEventListener("submit", handleFormSubmit, false);
+}, false);
