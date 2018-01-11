6b9bc0de2e641f7badc237d0c1b6077bc01b867b
Gary Chi <therealgary>
2018-01-11 10:15:17 -0500

    quality of the form

--- goodThings.js
+++ goodThings.js
@@ -1,4 +1,20 @@
+var globalTimer;
+
 document.addEventListener('DOMContentLoaded', function (e) {
-    //check for input errors
+    //form validation
+    var inNetID = document.getElementById("inputNetID");
+    inNetID.minLength = 2;
+    inNetID.maxLength = 6;
+    inNetID.pattern = "[a-zA-Z0-9]+";
     
+    var inItem = document.getElementById("inputItem");
+    inItem.minLength = 1;
+
+    //timer
+    globalTimer = setTimeout(hideNotif, 3000);
+    clearTimeout(globalTimer);
 });
+
+function hideNotif() {
+    document.getElementById("notif").style.display = "none";
+}
