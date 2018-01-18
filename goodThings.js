d43437fda05396c66d95d3e5bd77d189c9957e13
stcnyu <34110931+stcnyu@users.noreply.github.com>
2018-01-18 16:23:18 -0500

    Update goodThings.js

--- goodThings.js
+++ goodThings.js
@@ -71,7 +71,7 @@ function clearBody() {
 
 function showAbout() {
     var aboutEl = document.createElement("div");
-    aboutEl.innerHTML = 'NYU LaGuardia Co-op<br><img src="lenda.png"><br>version 1.0.1<br>https://github.com/stcnyu/';
+    aboutEl.innerHTML = 'NYU LaGuardia Co-op<br>version 1.0.1<br>https://github.com/stcnyu/';
     aboutEl.style.textAlign = "left";
     aboutEl.style.lineHeight = 1.5;
     aboutEl.style.width = "24em";
